30c1bbbc91b3427eb2e530e82cb41757a374b703
soon <soonotieno@gmail.com>
2021-02-26 18:01:04 +0900

    media setting

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     title = models.TextField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/%Y%m%d/', blank=True)
+
     created = models.DateTimeField()
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)

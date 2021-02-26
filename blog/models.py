829af3b283580f84dbf51dd6f2f77a22c40c67b2
soon <soonotieno@gmail.com>
2021-02-26 12:40:32 +0900

    blog app - Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField(max_length=30)
+    content = models.TextField()
+
+    created = models.DateTimeField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)

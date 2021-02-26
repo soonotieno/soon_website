447b7499858a8f9ed3b452581b23c9dc66255d6d
soon <soonotieno@gmail.com>
2021-02-26 13:55:31 +0900

    time : seoul, post : str

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,6 @@ class Post(models.Model):
     created = models.DateTimeField()
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.title, self.author)

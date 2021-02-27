b249b82e01075554025fbcf43d0ba98dec38a9b7
soon <soonotieno@gmail.com>
2021-02-27 15:15:08 +0900

    initilized post_detail test

--- blog/tests.py
+++ blog/tests.py
@@ -7,10 +7,26 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+def create_post(title, content, author):
+    blog_post = Post.objects.create(
+        title=title,
+        content=content,
+        created=timezone.now(),
+        author=author
+    )
+
+    return blog_post
+
+
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
         self.author_000 = User.objects.create(username='smith', password='nopassword')
+
+    def check_navbar(self, soup):
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
 
     def test_post_list(self):
         response = self.client.get('/blog/')
@@ -19,22 +35,20 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         title = soup.title
 
-        print(title.text)
-
         self.assertEqual(title.text, 'Blog')
 
-        navbar = soup.find('div', id='navbar')
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.check_navbar(soup)
+        # navbar = soup.find('div', id='navbar')
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About me', navbar.text)
 
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
-        post_000 = Post.objects.create(
+        post_000 = create_post(
             title='The first post',
             content='Hello World. We Are The World',
-            created=timezone.now(),
-            author=self.author_000
+            author=self.author_000,
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -45,3 +59,24 @@ class TestView(TestCase):
         body = soup.body
         self.assertNotIn('아직 게시물이 없습니다.', body.text)
         self.assertIn(post_000.title, body.text)
+
+    def test_post_detail(self):
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We Are The World',
+            author=self.author_000,
+        )
+
+        self.assertGreater(Post.objects.count(), 0)
+        post_000_url = post_000.get_absolute_url()
+        self.assertEqual(post_000_url, '/blog/{}/'.format(post_000.pk))
+
+        response = self.client.get(post_000_url)
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
+
+        self.check_navbar(soup)

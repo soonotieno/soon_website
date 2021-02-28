bfa7e745a00efbca0eedb88249708621a07f6d1c
soon <soonotieno@gmail.com>
2021-02-28 12:14:35 +0900

    tag model tested

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,14 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.TextField(max_length=30)
     content = models.TextField()
@@ -29,6 +37,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return '{} - {}'.format(self.title, self.author)

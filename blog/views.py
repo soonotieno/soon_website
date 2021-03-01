b6ee614bda1df5df7e3afb053b008852e221e568
soon <soonotieno@gmail.com>
2021-03-01 15:56:10 +0900

    we can delete comment

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CommentForm
@@ -112,6 +112,22 @@ def new_comment(request, pk):
             return redirect(comment.get_absolute_url())
     else:
         return redirect('/blog/')
+
+
+def delete_comment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    post = comment.post
+    if request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url() + '#comment-list')
+    else:
+        return redirect('/blog/')
+
+
+
+
+
+
 
 # def post_detail(request, pk):
 #     blog_post = Post.objects.get(pk=pk)

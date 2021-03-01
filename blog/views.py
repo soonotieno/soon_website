fafed27ec1b53bbffcabe4bcf5c25b1b7330d48f
soon <soonotieno@gmail.com>
2021-03-01 19:09:02 +0900

    delete comment with CBV: (Test not applied)

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Post, Category, Tag, Comment
-from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CommentForm
 
@@ -114,19 +114,27 @@ def new_comment(request, pk):
         return redirect('/blog/')
 
 
-def delete_comment(request, pk):
-    comment = Comment.objects.get(pk=pk)
-    post = comment.post
-    if request.user == comment.author:
-        comment.delete()
-        return redirect(post.get_absolute_url() + '#comment-list')
-    else:
-        return redirect('/blog/')
+class CommentDelete(DeleteView):
+    model = Comment
 
+    def get_object(self, queryset=None):
+        comment = super(CommentDelete, self).get_object()
+        if comment.author != self.request.user:
+            raise PermissionError('Comment 삭제 권한이 없습니다.')
+        return comment
 
+    def get_success_url(self):
+        post = self.get_object().post
+        return post.get_absolute_url() + '#comment-list'
 
-
-
+# def delete_comment(request, pk):
+#     comment = Comment.objects.get(pk=pk)
+#     post = comment.post
+#     if request.user == comment.author:
+#         comment.delete()
+#         return redirect(post.get_absolute_url() + '#comment-list')
+#     else:
+#         return redirect('/blog/')
 
 
 # def post_detail(request, pk):
